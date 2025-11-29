1009e937383ef7b36795efb0454656cb87c7c95e
Pranav Sakhare <pranavsakhare@Pranavs-MacBook-Air.local>
2025-11-29 16:48:54 +0530

    adding day1,2 files

--- Day2/main.tf
+++ Day2/main.tf
@@ -1,1 +1,5 @@
-
+resource "aws_instance" "name" {
+  ami = "ami-0d176f79571d18a8f"
+  instance_type = "t2.micro"
+  key_name = "keypair"
+}
---
+++ Day2/variables.tf
@@ -0,0 +1,16 @@
+variable "ami_id" {
+    description = "creataing variable for ami id"
+    type        = string
+    default     = ""
+}
+
+variable "type" {
+    description = "creation of instance type"
+    type        = string
+    default     = ""
+}
+variable "keypair_name" {
+    description = "creation of keypair name"
+    type        = string
+    default     = ""
+}
