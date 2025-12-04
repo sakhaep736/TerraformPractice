fd9434ad98c331a5f9c770098888afbf87ed916a
Pranav Sakhare <pranavsakhare@Pranavs-MacBook-Air.local>
2025-12-04 23:17:09 +0530

    updated the file

--- Day4_s3_backend/backend.tf
+++ Day4_s3_backend/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket="Pranav-sakhare-terraform-bucket-2025-backend-state-file"
-    key="terraform.tfstate"
-    region="us-east-1"
+    bucket = "pranav-sakhare-terraform-bucket-2025-backend-state-file"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
   }
 }
--- Day4_s3_state_file/main.tf
+++ Day4_s3_state_file/main.tf
@@ -2,6 +2,7 @@ resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
+  
 
   tags = {
     Name = "my-vpc"
@@ -9,6 +10,5 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_s3_bucket" "name" {
-     bucket="Pranav-sakhare-terraform-bucket-2025-backend-state-file"
-    region="us-east-1"
+     bucket = "pranav-sakhare-terraform-bucket-2025-backend-state-file"
 }
