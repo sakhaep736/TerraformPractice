6137bb343168dbe2fa5d42759ce16ea866c2ce75
Pranav Sakhare <pranavsakhare@Pranavs-MacBook-Air.local>
2025-12-05 15:53:46 +0530

    updated final

--- Day4_s3_backend/backend.tf
+++ Day4_s3_backend/backend.tf
@@ -1,7 +1,7 @@
 terraform {
-  backend "s3" {
-    bucket = "pranav-tf-us-east1"
-    key    = "terraform.tfstate"
-    region = "us-east-1a"
-  }
+  # backend "s3" {
+  #   bucket = "pranav-tf-us-east1"
+  #   key    = "terraform.tfstate"
+  #   region = "us-east-1"
+  # }
 }
--- Day4_s3_backend/main.tf
+++ Day4_s3_backend/main.tf
@@ -2,13 +2,12 @@ resource "aws_instance" "name" {
   ami = "ami-0fa3fe0fa7920f68e"
   instance_type = "t2.micro"
   key_name = "key"
-  region = "us-east-1a"
+  region = "us-east-1"
   tags = {
     Name = "test_state_file"
   }
 }
 
 resource "aws_s3_bucket" "name" {
-  bucket="pranav-tf-us-east1"
-  region = "us-east-1a"
+  bucket = "pranav-tf-us-east1"
 }
